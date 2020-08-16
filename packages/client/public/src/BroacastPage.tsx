ce83d61a08cd62c1da767c1776a3ae762b31bf58
Moroine Bentefrit <moroine.bentefrit@gmail.com>
2020-08-16 11:56:49 +0200

    Add testing for stopping Broadcast

--- packages/client/public/src/BroacastPage.tsx
+++ packages/client/public/src/BroacastPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { Broadcast, useSDK, Stage } from "../../src";
 import { useParams } from "react-router-dom";
 import { useApi } from "./useApi";
@@ -7,15 +7,30 @@ export default function BroadcastPage(): JSX.Element {
   const { token } = useParams();
   const endpoint = useApi().ws;
   const settings = { endpoint, token };
+  const [stopped, setStopped] = useState(false);
 
   const state = useSDK(settings);
+  const onToggleStop = useCallback(() => {
+    setStopped(!stopped);
+  }, [stopped]);
 
   if (!state.loaded) {
     return <div>Loading...</div>;
   }
 
+  if (stopped) {
+    return (
+      <div>
+        Stopped <button onClick={onToggleStop}>Restart</button>
+      </div>
+    );
+  }
+
   return (
     <div>
+      <div>
+        <button onClick={onToggleStop}>Stop</button>
+      </div>
       <Broadcast sdk={state.sdk} />
       <Stage sdk={state.sdk} />
     </div>
