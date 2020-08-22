3ddb73741cc5f0d5f7b109934a50f428a95fca1a
Moroine Bentefrit <moroine.bentefrit@gmail.com>
2020-08-22 19:20:46 +0200

    feat(client): add conventional commit

--- packages/client/release.config.js
+++ packages/client/release.config.js
@@ -4,4 +4,18 @@ module.exports = {
     { name: "develop", prerelease: true, prerelease: "beta" },
   ],
   repositoryUrl: "https://github.com/amfa-team/picnic-sfu.git",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "semantic-release-slack-bot",
+      {
+        notifyOnSuccess: true,
+        notifyOnFail: true,
+        markdownReleaseNotes: true,
+      },
+    ],
+  ],
 };
