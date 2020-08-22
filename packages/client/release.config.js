d1eb33dbd6b85500a03c5ab16a159af0cd171c3f
Moroine Bentefrit <moroine.bentefrit@gmail.com>
2020-08-22 17:30:00 +0200

    !feat(client): add deployment

---
+++ packages/client/release.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  branches: [
+    "master",
+    { name: "develop", prerelease: true, prerelease: "beta" },
+  ],
+  repositoryUrl: "git@github.com:amfa-team/picnic-sfu.git",
+};
