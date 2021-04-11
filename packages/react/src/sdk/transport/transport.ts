a52d6e7102f722ce1710648c629afd13e3d941d8
Moroine <moroine.bentefrit@gmail.com>
2021-04-11 09:15:37 +0200

    feat: expose sdk

--- packages/react/src/sdk/transport/transport.ts
+++ packages/react/src/sdk/transport/transport.ts
@@ -19,52 +19,52 @@ export type TransportEvents = {
 };
 
 export class PicnicTransport extends EventTarget<TransportEvents, "strict"> {
-  #ws: PicnicWebSocket;
+  _ws: PicnicWebSocket;
 
-  #device: PicnicDevice;
+  _device: PicnicDevice;
 
-  #type: TransportType;
+  _type: TransportType;
 
-  #transport: types.Transport | null = null;
+  _transport: types.Transport | null = null;
 
-  #state: TransportState = "initial";
+  _state: TransportState = "initial";
 
   constructor(ws: PicnicWebSocket, device: PicnicDevice, type: TransportType) {
     super();
 
-    this.#ws = ws;
-    this.#device = device;
-    this.#type = type;
+    this._ws = ws;
+    this._device = device;
+    this._type = type;
   }
 
   getState(): TransportState {
-    return this.#state;
+    return this._state;
   }
 
-  #setState = (state: TransportState): void => {
-    this.#state = state;
+  _setState = (state: TransportState): void => {
+    this._state = state;
     const evt = new PicnicEvent("state:change", state);
     this.dispatchEvent(evt);
   };
 
   async destroy(): Promise<void> {
     // TODO: notify when is user initiated
-    this.#transport?.close();
+    this._transport?.close();
   }
 
   getId(): string {
-    if (this.#transport === null) {
+    if (this._transport === null) {
       throw new PicnicError(
         "PicnicTransport.getId: transport is not loaded yet",
         null,
       );
     }
 
-    return this.#transport.id;
+    return this._transport.id;
   }
 
   async load(): Promise<void> {
-    this.#setState("creating");
+    this._setState("creating");
 
     try {
       const {
@@ -73,9 +73,9 @@ export class PicnicTransport extends EventTarget<TransportEvents, "strict"> {
         iceCandidates,
         dtlsParameters,
         sctpParameters,
-      } = await this.#ws.send<ConnectionInfo>(
+      } = await this._ws.send<ConnectionInfo>(
         "/sfu/connect/init",
-        this.#device.getInitConnectionParams(this.#type),
+        this._device.getInitConnectionParams(this._type),
       );
 
       const params = {
@@ -87,25 +87,25 @@ export class PicnicTransport extends EventTarget<TransportEvents, "strict"> {
         iceServers: [],
       };
 
-      this.#transport = this.#device.createMediasoupTransport(
-        this.#type,
+      this._transport = this._device.createMediasoupTransport(
+        this._type,
         params,
       );
 
       // eslint-disable-next-line @typescript-eslint/no-misused-promises
-      this.#transport.on("connect", this.#onConnect);
-      this.#transport.on(
+      this._transport.on("connect", this._onConnect);
+      this._transport.on(
         "connectionstatechange",
-        this.#onConnectionStateChange,
+        this._onConnectionStateChange,
       );
-      if (this.#type === "send") {
+      if (this._type === "send") {
         // eslint-disable-next-line @typescript-eslint/no-misused-promises
-        this.#transport.on("produce", this.#onProduce);
+        this._transport.on("produce", this._onProduce);
       }
 
-      this.#setState("connected");
+      this._setState("connected");
     } catch (e) {
-      this.#setState("error");
+      this._setState("error");
       throw new PicnicError("PicnicTransport.load: fail", e);
     }
   }
@@ -113,44 +113,44 @@ export class PicnicTransport extends EventTarget<TransportEvents, "strict"> {
   async produce(
     options: types.ProducerOptions & { track: MediaStreamTrack },
   ): Promise<types.Producer> {
-    if (this.#transport === null || this.#type !== "send") {
+    if (this._transport === null || this._type !== "send") {
       throw new PicnicError(
         "PicnicTransport.produce: requires a send transport",
         null,
       );
     }
 
-    return this.#transport.produce(options);
+    return this._transport.produce(options);
   }
 
   async consume(options: types.ConsumerOptions): Promise<types.Consumer> {
-    if (this.#transport === null || this.#type !== "recv") {
+    if (this._transport === null || this._type !== "recv") {
       throw new PicnicError(
         "PicnicTransport.consume: requires a recv transport",
         null,
       );
     }
 
-    return this.#transport.consume(options);
+    return this._transport.consume(options);
   }
 
-  #onConnect = async (
+  _onConnect = async (
     { dtlsParameters }: { dtlsParameters: types.DtlsParameters },
     callback: () => void,
     errback: (e: Error) => void,
   ): Promise<void> => {
     try {
-      if (!this.#transport) {
+      if (!this._transport) {
         throw new PicnicError(
           "PicnicTransport.ensureTransport: transport must exists",
           null,
         );
       }
       const connectParams: ConnectParams = {
-        transportId: this.#transport.id,
+        transportId: this._transport.id,
         dtlsParameters,
       };
-      await this.#ws.send<ConnectionInfo>("/sfu/connect/create", connectParams);
+      await this._ws.send<ConnectionInfo>("/sfu/connect/create", connectParams);
       callback();
     } catch (e) {
       captureException(e);
@@ -158,19 +158,19 @@ export class PicnicTransport extends EventTarget<TransportEvents, "strict"> {
     }
   };
 
-  #onConnectionStateChange = (e: unknown): void => {
+  _onConnectionStateChange = (e: unknown): void => {
     if (e === "disconnected") {
-      this.#setState("disconnected");
+      this._setState("disconnected");
     } else if (e === "connected") {
-      this.#setState("connected");
+      this._setState("connected");
     } else if (e === "connecting") {
-      this.#setState("connecting");
+      this._setState("connecting");
     } else {
       console.warn("transport connection changes", e);
     }
   };
 
-  #onProduce = async (
+  _onProduce = async (
     {
       kind,
       rtpParameters,
@@ -179,19 +179,19 @@ export class PicnicTransport extends EventTarget<TransportEvents, "strict"> {
     errback: (e: Error) => void,
   ): Promise<void> => {
     try {
-      if (!this.#transport) {
+      if (!this._transport) {
         throw new PicnicError(
           "PicnicTransport.ensureTransport: transport must exists",
           null,
         );
       }
       const req: SendParams = {
-        transportId: this.#transport.id,
+        transportId: this._transport.id,
         kind,
         rtpParameters,
       };
 
-      const producerId = await this.#ws.send<string>("/sfu/send/create", req);
+      const producerId = await this._ws.send<string>("/sfu/send/create", req);
 
       callback({ id: producerId });
     } catch (error) {
